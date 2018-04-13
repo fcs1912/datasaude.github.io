c8a39aa29f6582b31263a5ee109ec43960e139f5
Anderson Jr <amf.jr6@gmail.com>
2018-04-13 03:44:06 -0300

    Site com algumas implementações

---
+++ site/assets/js/graficos.js
@@ -0,0 +1,123 @@
+graficos = {
+  initDocumentationCharts: function () {
+    dataDailySalesChart = {
+      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      series: [
+        [12, 17, 7, 17, 23, 18, 38]
+      ]
+    };
+
+    optionsDailySalesChart = {
+      lineSmooth: Chartist.Interpolation.cardinal({
+        tension: 0
+      }),
+      low: 0,
+      high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      chartPadding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
+    }
+
+    var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+  },
+
+  initDashboardPageCharts: function () {
+
+    var dataPreferences = {
+      series: [
+        [25, 30, 20, 25]
+      ]
+    };
+
+    var optionsPreferences = {
+      donut: true,
+      donutWidth: 40,
+      startAngle: 0,
+      total: 100,
+      showLabel: false,
+      axisX: {
+        showGrid: false
+      }
+    };
+
+    Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+
+    Chartist.Pie('#chartPreferences', {
+      labels: ['53%', '36%', '11%'],
+      series: [53, 36, 11]
+    });
+
+
+    var dataSales = {
+      labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+      series: [
+        [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+      ]
+    };
+
+    var optionsSales = {
+      lineSmooth: false,
+      low: 0,
+      high: 900,
+      chartPadding: 0,
+      showArea: false,
+      height: "250px",
+      axisX: {
+      showGrid: false,
+    },
+    axisY: {
+      showGrid: false,
+    },
+    lineSmooth: Chartist.Interpolation.simple({
+      divisor: 10
+    }),
+      showLine: true,
+      showPoint: true,
+      fullWidth: false
+    };
+
+    var responsiveSales = [
+      ['screen and (max-width: 640px)', {
+        axisX: {
+          labelInterpolationFnc: function (value) {
+            return value[0];
+          }
+        }
+      }]
+    ];
+
+    var chartHours = Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
+
+    var data = {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      series: [
+        [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
+        [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
+      ]
+    };
+
+    var options = {
+      seriesBarDistance: 10,
+      axisX: {
+        showGrid: false
+      },
+      height: "245px"
+    };
+
+    var responsiveOptions = [
+      ['screen and (max-width: 640px)', {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function (value) {
+            return value[0];
+          }
+        }
+      }]
+    ];
+
+    var chartActivity = Chartist.Bar('#chartActivity', data, options, responsiveOptions);
+  },
+}
