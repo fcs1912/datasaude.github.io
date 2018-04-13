ae331ebf1daf8e0b9265a2e1b88fa79888978e76
Henrique Barbosa <mudgenone@gmail.com>
2018-04-13 18:15:01 -0300

    Previsao das possíveis doenças

---
+++ previsao/possivelDoenca.js
@@ -0,0 +1,20 @@
+[
+    ['São José do Campestre', 'Leiomioma subseroso do útero', 'Trabalho de parto e parto complicados com sofrimento fetal', 'Outras anormalidades da contração uterina', 'Desconforto respiratório não especificado do recém nascido', 'Fratura da rótula [patela]'],
+    ['Trairi', 'Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['Gameleira', 'Outras enterites virais', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['Areia','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['São José da Passagem', 'Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Cistocele'],
+    ['Barão de Serra Branca','Outras enterites virais', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['Belo Horizonte','Outras enterites virais', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['Santa Teresa', 'Outras enterites virais', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['Escada','Outras enterites virais', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['São José de Marimbas','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['Inajá','Outras enterites virais', 'Parto espontâneo cefálico', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda', 'Cistocele'],
+    ['São Gonçalo','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['Rincão do Meio', 'Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['Barra de Guabiraba','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['Triunfo','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['São Caetando','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['Barra do Camaratuba','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda'],
+    ['Tamandaré','Outras enterites virais', 'Parto espontâneo cefálico', 'Parto único espontâneo', 'Hérnia inguinal unilateral ou não especificada', 'Calculose da vesícula biliar com colicistite aguda']
+]
